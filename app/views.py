bcad5702deb459d9f23c8b05850df37bfbdbf75d
Seenu9347 <seenaiahchitra111@gmail.com>
2023-11-21 11:48:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def seenu(request):
+    return render(request,'seenu.html')
+
+def steven(request):
+    return render(request,'steven.html')
+
+def seenu1(request):
+    return render(request,'seenu1.html')
+
+def steven1(request):
+    return render(request,'steven1.html')
